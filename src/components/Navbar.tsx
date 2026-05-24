578e381bbf4b763b30639601ce296c7d1fe78c41
kusdianto kusuma rahman <studiozoft@gmail.com>
2026-05-25 04:10:18 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar() {
               </div>
             )}
             <span className="font-bold text-xl tracking-tight text-slate-900">
-              {settings?.namaSekolah || 'SDN Harapan Bangsa'}
+              {settings?.namaSekolah || 'UPTD SDN MRECAH 2'}
             </span>
           </Link>
 
